140a0fc112f054ab95e208dfbaf66a0e8888b93e
carlitoselmago <ccarbonell@gmail.com>
2022-09-18 19:38:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ while True:
     #check for video loop
     remaining=videolen-media_player.get_time()
     print("rem",remaining)
-    if remaining<200: 
+    if remaining<300: 
         #if remaining is less than 200ms (rarelly gives 0ms)
         media_player.set_time(1)
 
